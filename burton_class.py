27f12c37daf74e725310ba38294b212b3003b903
Dotan Cohen <github-com-2010.04@dotancohen.com>
2016-01-20 15:32:13 +0200

    Implement get_settings_file()

--- burton_class.py
+++ burton_class.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 
 
@@ -16,12 +17,45 @@ class Burton(object):
 
 	def get_settings_file(self):
 
-		return True
+		config_dir = os.path.realpath(os.path.expanduser('~/.burton'))
+
+		if not os.path.isdir(config_dir):
+			print("Creating settings directory: %s ... " % (config_dir,), end='')
+			os.mkdir(config_dir)
+			if not os.path.isdir(config_dir):
+				raise Exception("Cannot create settings direcroty!")
+			print("Done!")
+
+		config_file = os.path.join(config_dir, 'burtonrc')
+
+		if os.path.isfile(config_file):
+			fh = open(config_file, 'w')
+		else:
+			print("Creating settings file: %s ... " % (config_file,), end='')
+			fh = open(config_file, 'w')
+			open(config_file, 'w')
+			fh.write('')
+			fh.flush()
+			if not os.path.isfile(config_file):
+				raise Exception("Cannot create settings file!")
+			print("Done!")
+
+		return fh
+
+
+
+	def get_settings_default(self):
+		# Not yet used
+
+		settings = {
+			'example-files': 'example-files-DEFAULT'
+		}
+
+		return settings
 
 
 
 	def get_user_setting(self, key):
-
-
+		# Not yet used
 		return value
 
